5fc074a6df88edd15293f2d16742ad1b7849589a
Lyubo03 <58880537+Lyubo03@users.noreply.github.com>
2021-03-20 19:28:37 +0200

    Add files via upload

---
+++ PMG.Tests/Factory/PMGDbContextInMemoryFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace PMG.Tests.Factory
+{
+    using Microsoft.EntityFrameworkCore;
+    using PMG.Data;
+    using System;
+    public class PMGDbContextInMemoryFactory
+    {
+        public static PMGDbContext CreateDbContext()
+        {
+            var options = new DbContextOptionsBuilder<PMGDbContext>()
+                  .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                  .Options;
+
+            return new PMGDbContext(options);
+        }
+    }
+}
